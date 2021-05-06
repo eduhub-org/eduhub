6fc94aff4ab624ebb5a35a981694729c7d23ca96
Oliver Kwast <ok@unscoped.dev>
2021-05-06 17:39:10 +0200

    Added tailwind to the project

--- frontend/components/Page.tsx
+++ frontend/components/Page.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from "react";
 
-import styles from "./Page.module.css";
-
 export const Page: FC = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.page}>{children}</div>
-    </div>
+    <>
+      <main className="flex-column items-center">
+        <div className="flex max-w-screen-xl mx-auto border-2">{children}</div>
+      </main>
+    </>
   );
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,37 +3,24 @@ import { FC } from "react";
 import { useTranslation } from "react-i18next";
 
 import { Page } from "../components/Page";
-import styles from "../styles/Home.module.css";
 
 const Home: FC = () => {
   const { t } = useTranslation();
 
   return (
-    <Page>
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <Page>
+        <h1>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p>{t("header.signin")}</p>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </Page>
+      </Page>
+    </>
   );
 };
 
