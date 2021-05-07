9c4cfde871c4a78e303f9f2cb466b2142bcc2a40
Oliver Kwast <oliver.kwast@bitglow.de>
2021-05-07 10:52:57 +0200

    Turned on tailwinds jit mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  plugins: [],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
 };
