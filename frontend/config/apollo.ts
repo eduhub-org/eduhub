37f034b47f3c5b10cd4117c979d94816f941b743
Oliver Kwast <oliver.kwast@bitglow.de>
2021-05-31 14:52:56 +0200

    Proxy api through next js to circumvent cors errors

--- frontend/config/apollo.ts
+++ frontend/config/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: process.env.GRAPHQL_URI ?? "http://localhost:8080/v1/graphql",
+  uri: "/data/v1/graphql",
   cache: new InMemoryCache(),
 });
