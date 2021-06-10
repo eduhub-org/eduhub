58792c049010f239fccdfb84a5842ed53f968d67
Oliver Kwast <oliver.kwast@bitglow.de>
2021-06-10 10:36:29 +0200

    10: Improved page layout to not have padding when bigger than xl

--- frontend/components/common/PageBlock.tsx
+++ frontend/components/common/PageBlock.tsx
@@ -1,5 +1,5 @@
 import { Children, FC } from "react";
 
 export const PageBlock: FC = ({ children }) => {
-  return <div className="mx-6">{children}</div>;
+  return <div className="mx-6 xl:mx-0">{children}</div>;
 };
