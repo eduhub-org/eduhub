2e495462f754bd345f9d114844bb4bb533a701b4
Eric Stumper <eric.stumper@gmail.com>
2022-05-10 15:08:39 +0200

    modifies keycloak gcsfuse command flags

--- keycloak/start.sh
+++ keycloak/start.sh
@@ -3,7 +3,7 @@
 # Use gcsfuse to mount network share
 
 echo "Mounting GCS Fuse."
-gcsfuse --debug_gcs --debug_fuse edu-hub-production-keycloak-seeds /mnt/bucket
+gcsfuse -o rw,allow_other -file-mode=777 -dir-mode=777 --debug_gcs --debug_fuse --debug_http --debug_fs --debug_mutex --implicit-dirs edu-hub-production-keycloak-seeds /mnt/bucket
 echo "Mounting completed."
 
 /opt/keycloak/bin/kc.sh import --dir "/mnt/bucket"
