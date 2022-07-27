5f45528d02618d50b0e95006d86f6de1f123b543
Christoph Krüger <christoph@opencampus.sh>
2022-07-27 14:17:03 +0200

    updateFromKeycloak function cleanup and called from frontend

--- frontend/components/LoginButton.tsx
+++ frontend/components/LoginButton.tsx
@@ -1,9 +1,16 @@
 import { useKeycloak } from "@react-keycloak/ssr";
-import { KeycloakInstance } from "keycloak-js";
+import { KeycloakInstance, KeycloakTokenParsed } from "keycloak-js";
 import { useRouter } from "next/router";
 import { FC, useCallback, useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { gql, useMutation } from '@apollo/client';
 import { Button } from "./common/Button";
+
+const UPDATE_USER = gql`
+  mutation update_User($id: ID!) {
+    updateFromKeycloak(userid: $id)
+  }
+`;
 
 export const useLogin = () => {
   const { keycloak } = useKeycloak<KeycloakInstance>();
@@ -13,6 +20,8 @@ export const useLogin = () => {
       redirectUri: window.location.href,
     });
     if (!url) return;
+
+    
     
     router.push(new URL(url));
   }, [keycloak, router]);
@@ -20,6 +29,19 @@ export const useLogin = () => {
   return performLogin;
 };
 export const LoginButton: FC = () => {
+  const { keycloak } = useKeycloak<KeycloakInstance>();
+  const [updateUser, { data, error }] = useMutation(UPDATE_USER);
+
+  useEffect(() => {
+    if (keycloak !== undefined) {
+      keycloak.onAuthSuccess = () => {
+        const parsedToken: KeycloakTokenParsed | undefined = keycloak?.tokenParsed;
+
+        updateUser({ variables: { id: parsedToken?.sub} });
+      }
+    }
+  },[]); 
+
   const { t } = useTranslation();
   const performLogin = useLogin();
 
