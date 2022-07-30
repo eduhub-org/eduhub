0f780ffdc56136fcdb51a4e5fddcafd09bb17b85
cclausen <c.clausen@pct-digital.de>
2022-07-30 20:42:01 +0200

    make the images work

--- rent-a-scientist/components/ScientistAvatar.tsx
+++ rent-a-scientist/components/ScientistAvatar.tsx
@@ -1,12 +1,32 @@
 import { FC } from "react";
 import { AllScientistOffers_ScientistOffer_ScientistOfferRelations_Scientist } from "../queries/__generated__/AllScientistOffers";
-import appLogo from "../public/images/logo_wzk_300x340.png";
 
 interface IProps {
   scientist: AllScientistOffers_ScientistOffer_ScientistOfferRelations_Scientist;
   subject: string;
   logo?: string;
   className?: string;
+}
+
+const umlautMap: any = {
+  '\u00dc': 'UE',
+  '\u00c4': 'AE',
+  '\u00d6': 'OE',
+  '\u00fc': 'ue',
+  '\u00e4': 'ae',
+  '\u00f6': 'oe',
+  '\u00df': 'ss',
+}
+
+function replaceUmlaute(str: string) {
+  return str
+    .replace(/[\u00dc|\u00c4|\u00d6][a-z]/g, (a) => {
+      const big = umlautMap[a.slice(0, 1)];
+      return big.charAt(0) + big.charAt(1).toLowerCase() + a.slice(1);
+    })
+    .replace(new RegExp('['+Object.keys(umlautMap).join('|')+']',"g"),
+      (a) => umlautMap[a]
+    );
 }
 
 export const ScientistAvatar: FC<IProps> = ({
@@ -19,7 +39,7 @@ export const ScientistAvatar: FC<IProps> = ({
     <div className={className}>
       <div className="flex">
         <img
-          src={appLogo}
+          src={"/static/pics/" + replaceUmlaute(scientist.image?.trim().toLocaleLowerCase() || "99_wissenschaftzukunft") + ".jpg"}
           width={130}
           height={130}
           className="w-[100px] h-[100px] lg:w-[140px] lg:h-[140px]"
