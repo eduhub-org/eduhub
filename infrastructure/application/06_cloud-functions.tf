eafb8290a03d9b450d9b4ba803ca54ac15fd64cd
steffen74 <steffen@opencampus.sh>
2022-08-13 13:07:41 +0200

    further increase of sendMail max instances

--- infrastructure/application/06_cloud-functions.tf
+++ infrastructure/application/06_cloud-functions.tf
@@ -128,7 +128,7 @@ resource "google_cloudfunctions2_function" "send_mail" {
       HASURA_MAIL_PW               = var.hasura_mail_pw
       HASURA_MAIL_USER             = var.hasura_mail_user
     }
-    max_instance_count = 5
+    max_instance_count = 10
     available_memory   = "512M"
     timeout_seconds    = 60
   }
