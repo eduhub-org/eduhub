4ba09fbe3e94d53681d68aaef291f0c743e677a6
steffen74 <steffen@opencampus.sh>
2022-11-27 11:45:00 -0300

    Further completion of Attendance registration - insert and update mutation implemented name comparison using fuzzywuzzy package - date format extended to include time zone --> used times for zoom, EduHub and LimeSurvey must be double checked again

--- functions/callPythonFunction/pythonFunctions/checkAttendance.py
+++ functions/callPythonFunction/pythonFunctions/checkAttendance.py
@@ -8,12 +8,13 @@ import api
 import pandas as pd
 from flask import escape
 from flask import jsonify
+from fuzzywuzzy import fuzz
 
 
 def checkAttendance():
     """ For all sessions for which the attendance hasn't been checked yet
     (no attendance data stored in the session table), gets the participations
-    from Zoom and Limesurvey and adds the individual attendances to the 
+    from Zoom and Limesurvey and adds the individual attendances to the
     attandance table (for each registered and correctly identified person)
     and the Session table (a JSON including all recorded participants of the
     session).
@@ -43,19 +44,25 @@ def checkAttendance():
         offline_attendance['source'] = 'LIMESURVEY'
         attendance_data = pd.concat([attendance_data, offline_attendance])
 
+        attendance_data['sessionId'] = session['id']
+        # reset the index for later argmax search in `get_attendance_data()`
+        attendance_data.reset_index(drop=True, inplace=True)
+
         # Get course participants
-        course_participants = edu.get_participants_from_session(
+        course_participants = edu.get_course_participants_from_session_id(
             session_id=session['id'])
 
         # Matching each course participant with the names in the attendance data
         # and storing the partipant's attendance in the Attendance table
-        for participant in course_participants:
-            participant_attendance = get_participant_attendance(
-                participant, attendance_data)
-            edu.mutate_attendance(participant_attendance)
+        pd.options.mode.chained_assignment = None  # default='warn'
+
+        for p in range(len(course_participants)):
+            course_participant_attendance = get_attendance_data(
+                course_participants.iloc[p, :], attendance_data)
+            edu.insert_attendance(course_participant_attendance)
 
         # Storing JSON of complete attendance_data in Session table
-        edu.mutate_session_attendanceData(session['id'], attendance_data)
+        edu.update_session_attendanceData(attendance_data)
 
     return sessions
 
@@ -76,11 +83,14 @@ def get_offline_session_attendance(session):
         # Rename variables from LimeSurvey
         survey_answers.rename(columns={'datestamp': 'joinDateTime', 'N1': 'firstName',
                               'N2': 'lastName', 'Place': 'location'}, inplace=True)
+        # New column for full name
+        survey_answers['name'] = survey_answers['firstName'] + \
+            ' ' + survey_answers['lastName']
         # Format time variable
         survey_answers['joinDateTime'] = lms.to_datetime(
             survey_answers['joinDateTime'])
         # Filter to only those answers who registered during the correct time
-        session_attendances = survey_answers.iloc[(survey_answers['joinDateTime'] >= (session['startDateTime'] - pd.Timedelta(hours=1))) & (
+        session_attendances = survey_answers[(survey_answers['joinDateTime'] >= (session['startDateTime'] - pd.Timedelta(hours=1))) & (
             survey_answers['joinDateTime'] <= (session['endDateTime'] + pd.Timedelta(hours=1)))]
 
         # TODO If offline address is provided in the session filter the attendances
@@ -89,7 +99,46 @@ def get_offline_session_attendance(session):
         return session_attendances
 
 
-def get_participant_attendance(participant, attendance_data):
+def get_attendance_data(participant, attendance_data):
     """lorem  ipsum"""
-    participant_attendance = []
-    return participant_attendance
+    participant_full_name = participant['firstName'] + \
+        ' ' + participant['lastName']
+    attendance_data['score'] = [fuzz.token_sort_ratio(participant_full_name.lower(), name.lower())
+                                for name in attendance_data['name']]
+    participant_attendance = attendance_data.iloc[attendance_data['score'].idxmax(
+    )]
+    participant_attendance['userId'] = participant['id']
+    # Formatting of the missing/ null values to None for GraphQL
+    participant_attendance = participant_attendance.where(
+        pd.notnull(participant_attendance), None)
+    # Formatting of the int variables for GraphQL
+    participant_attendance['sessionId'] = int(
+        attendance_data['sessionId'][0])
+    if participant_attendance['interruptionCount'] is not None:
+        participant_attendance['interruptionCount'] = int(
+            participant_attendance['interruptionCount'])
+    if participant_attendance['duration'] is not None:
+        participant_attendance['duration'] = int(
+            participant_attendance['duration'])
+    # Formatting of the datetime variables for GraphQL
+    if participant_attendance['joinDateTime'] is not None:
+        participant_attendance['joinDateTime'] = str(
+            participant_attendance['joinDateTime'])
+    if participant_attendance['leaveDateTime'] is not None:
+        participant_attendance['leaveDateTime'] = str(
+            participant_attendance['leaveDateTime'])
+
+    if (participant_attendance['score'] >= 80):
+        participant_attendance['status'] = 'ATTENDED'
+        participant_attendance['recordedName'] = participant_attendance['name']
+    else:
+        participant_attendance['status'] = 'MISSED'
+        participant_attendance['recordedName'] = None
+    return pd.DataFrame(participant_attendance).transpose()
+
+    # Other potential functions are fuzz.ratio() and fuzz.partial_ratio()
+    # For testing purposes:
+    # data = [[fuzz.token_sort_ratio(participant_full_name.lower(), name.lower()), name]
+    #        for name in attendance_data['name']]
+    # m = pd.DataFrame(data=data, columns={'score', 'name'})
+    # mlist = list(m.iloc[m['score'].idxmax()])
