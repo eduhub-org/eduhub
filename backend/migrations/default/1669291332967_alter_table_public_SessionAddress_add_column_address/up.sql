b00b22269feef2fe036fffef1a2d33fbc6c78da9
steffen74 <steffen@opencampus.sh>
2022-12-02 19:57:08 -0300

    Default values added for new table fields

--- backend/migrations/default/1669291332967_alter_table_public_SessionAddress_add_column_address/up.sql
+++ backend/migrations/default/1669291332967_alter_table_public_SessionAddress_add_column_address/up.sql
@@ -1,2 +1,1 @@
-alter table "public"."SessionAddress" add column "address" text
- null;
+alter table "public"."SessionAddress" add column "address" text not null default '';
--- backend/migrations/default/1669291614465_alter_table_public_SessionAddress_add_column_type/up.sql
+++ backend/migrations/default/1669291614465_alter_table_public_SessionAddress_add_column_type/up.sql
@@ -1,2 +1,1 @@
-alter table "public"."SessionAddress" add column "type" text
- null;
+alter table "public"."SessionAddress" add column "type" text not null default 'FREETEXT_ADDRESS';
