5c111a39dfb30ed963a634ad2ec2fbfa947599fd
cclausen <c.clausen@pct-digital.de>
2022-12-21 19:35:15 +0100

    fix apollo for the nx frontend

--- bin/lint.sh
+++
@@ -1,2 +0,0 @@
-# Run the codegen inside the container
-docker exec -ti edu-hub_frontend_1 /usr/local/bin/yarn lint --fix
--- regenerate-apollo.sh
+++ regenerate-apollo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # How to call this:
-# sudo bash regenerate-apollo.sh <your_local_username>
+# sudo bash regenerate-apollo.sh $USER
 # The containers must be up for this!
 
 # Use this script to regenerate the apollo code 
@@ -17,8 +17,13 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-# Run the codegen inside the container
+# Run the codegen inside the container(s)
+# old edu
 docker exec -ti edu-hub_frontend_1 /usr/local/bin/yarn apollo:codegen
+# nx edu
+docker exec -ti edu-hub_edu-nx_1 npx nx run edu-hub:apollo
+# nx rent-a-scientist
+#docker exec -ti edu-hub_ras-nx_1 npx nx run edu-hub:apollo
 
 # Now the files are created as root, so need to make sure all the files in frontend are owned by the correct user again
 chown $1:$1 -R .
