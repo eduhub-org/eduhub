f4cf229217f5db84a9b84aa6f9baf3e4aa63f3e8
steffen74 <steffen@opencampus.sh>
2023-02-09 13:37:14 +0100

    Creation of additional tables to assign courses to groups

---
+++ backend/migrations/default/1675946023873_insert_into_public_CourseGroupOption/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."CourseGroupOption"("id", "title", "order", "created_at", "updated_at") VALUES (1, E'tech_coding', 1, E'2023-02-09T12:33:43.747732+00:00', E'2023-02-09T12:33:43.747732+00:00');
---
+++ backend/migrations/default/1675946107097_insert_into_public_CourseGroupOption/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."CourseGroupOption"("id", "title", "order", "created_at", "updated_at") VALUES (4, E'degree', 4, E'2023-02-09T12:35:07.006832+00:00', E'2023-02-09T12:35:07.006832+00:00');
