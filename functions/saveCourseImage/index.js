7a35065b5b15b87ca3beacd5f608242df3aa8ae0
Christoph Krüger <christoph@opencampus.sh>
2023-02-21 09:10:33 +0100

    example code to resize with imagemagick

--- functions/saveCourseImage/index.js
+++ functions/saveCourseImage/index.js
@@ -1,6 +1,8 @@
 const { Storage } = require("@google-cloud/storage");
 const { buildCloudStorage } = require((process.env.LIBRARY_PATH) ? process.env.LIBRARY_PATH : "./lib/cloud-storage");
 const storage = buildCloudStorage(Storage);
+const im = require("imagemagick");
+const fs = require("fs");
 
 /**
  * Responds to any HTTP request.
@@ -16,14 +18,30 @@ exports.saveCourseImage = async (req, res) => {
     const isPublic = true;
 
     const path = `public/courseid_${courseid}/cover_image/${filename}`;
-
+    
+    //test for resizing with imagemagick
+    /* const pathsmall = `public/courseid_${courseid}/cover_image/small_${filename}`;
+    
+    fs.writeFile(filename, content, 'base64', function(err) {
+      console.log(err);
+    });
+    
+    im.resize({
+      srcPath: filename,
+      dstPath: 'small-' + filename,
+      width:   256
+    }, function(err, stdout, stderr){
+      if (err) throw err;
+    });
+    */
+    
     const link = await storage.saveToBucket(
       path,
       req.headers.bucket,
       content,
       isPublic
     );
-
+    
     return res.json({
       path: link,
       google_link: link,
