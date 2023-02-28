83e8c0544859fa5c7ad4825b8c5f5b5823cf75a5
Eric Stumper <eric.stumper@gmail.com>
2023-02-28 16:34:04 +0100

    adds proxy access to internal frontend port for access to self

--- frontend-nx/dev.sh
+++ frontend-nx/dev.sh
@@ -4,6 +4,10 @@
 socat tcp-listen:28080,reuseaddr,fork tcp:keycloak:8080 &
 # proxy access to hasura for the updateUser callback
 socat tcp-listen:8080,reuseaddr,fork tcp:hasura:8080 &
+# proxy access to frontend for the refreshToken Api Route callback
+socat tcp-listen:5000,reuseaddr,fork tcp:localhost:4200 &
+# proxy access to rent-a-scientist frontend for the refreshToken Api Route callback
+socat tcp-listen:5001,reuseaddr,fork tcp:localhost:4201 &
 # make sure all libraries exist
 yarn
 # start the development servers for edu-hub and rent-a-scientist
