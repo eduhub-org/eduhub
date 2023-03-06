a3c9f4bf6bb46321c30230b048abbfb4cbb6cf37
steffen74 <steffen@opencampus.sh>
2023-03-06 18:13:40 +0100

    Profile view imports and layout back to new form

--- frontend-nx/apps/edu-hub/components/profile/ProfileOverview.tsx
+++ frontend-nx/apps/edu-hub/components/profile/ProfileOverview.tsx
@@ -1,4 +1,4 @@
-import { FC, InputHTMLAttributes, SelectHTMLAttributes } from 'react';
+import { FC, useRef, useCallback } from 'react';
 import {
   FormProvider,
   SubmitHandler,
@@ -6,19 +6,39 @@ import {
   useFormContext,
 } from 'react-hook-form';
 import { useSession } from 'next-auth/react';
+import useTranslation from 'next-translate/useTranslation';
+import { IconButton } from '@material-ui/core';
+import { MdUpload } from 'react-icons/md';
+
+import { parseFileUploadEvent } from '../../helpers/filehandling';
 
 import { Button } from '../common/Button';
 
 import { useAuthedMutation } from '../../hooks/authedMutation';
 import { useAuthedQuery } from '../../hooks/authedQuery';
+
 import { UPDATE_USER } from '../../queries/updateUser';
 import { USER } from '../../queries/user';
-import useTranslation from 'next-translate/useTranslation';
+import { SAVE_USER_PROFILE_IMAGE } from '../../queries/actions';
+import { UPDATE_USER_PROFILE_PICTURE } from '../../queries/updateUser';
 
+import type {
+  MutableRefObject,
+  InputHTMLAttributes,
+  SelectHTMLAttributes,
+} from 'react';
+import {
+  SaveUserProfileImage,
+  SaveUserProfileImageVariables,
+} from '../../queries/__generated__/SaveUserProfileImage';
 import {
   updateUserVariables,
   updateUser,
 } from '../../queries/__generated__/updateUser';
+import {
+  updateUserProfilePictureVariables,
+  updateUserProfilePicture,
+} from '../../queries/__generated__/updateUserProfilePicture';
 import { University_enum } from '../../__generated__/globalTypes';
 import { Employment_enum } from '../../__generated__/globalTypes';
 
@@ -229,56 +249,89 @@ const ProfileOverview: FC = () => {
         <>
           <FormProvider {...methods}>
             <form onSubmit={handleSubmit(onSubmit)}>
-              <FormFieldRow label={t('first-name')} name="firstName" required />
-              <FormFieldRow label={t('last-name')} name="lastName" required />
-              <FormFieldRow
-                label={t('email')}
-                name="email"
-                placeholder="name@example.com"
-                required
-                type="email"
-              />
-              <FormFieldRow
-                label={t('status')}
-                name="employment"
-                type="select"
-                options={employmentSelectFormOptions}
-              />
-              <FormFieldRow
-                label={t('university')}
-                name="university"
-                type="select"
-                options={universitySelectFormOptions}
-              />
-              <FormFieldRow
-                label={t('matriculation-number')}
-                name="matriculationNumber"
-              />
-              <FormFieldRow
-                label={t('external-profile')}
-                name="externalProfile"
-              />
+              <div className="flex flex-wrap">
+                <div className="w-1/2 pr-3">
+                  <FormFieldRow
+                    label={t('first-name')}
+                    name="firstName"
+                    required
+                  />
+                </div>
+                <div className="w-1/2 pl-3">
+                  <FormFieldRow
+                    label={t('last-name')}
+                    name="lastName"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap">
+                <div className="w-1/2 pr-3">
+                  <FormFieldRow
+                    label={t('email')}
+                    name="email"
+                    placeholder="name@example.com"
+                    required
+                    type="email"
+                  />
+                </div>
+                <div className="w-1/2 pl-3">
+                  <FormFieldRow
+                    label={t('status')}
+                    name="employment"
+                    type="select"
+                    options={employmentSelectFormOptions}
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap">
+                <div className="w-1/2 pr-3">
+                  <FormFieldRow
+                    label={t('university')}
+                    name="university"
+                    type="select"
+                    options={universitySelectFormOptions}
+                  />
+                </div>
+                <div className="w-1/2 pl-3">
+                  <FormFieldRow
+                    label={t('matriculation-number')}
+                    name="matriculationNumber"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap">
+                <div className="w-1/2 pr-3">
+                  <FormFieldRow
+                    label={t('external-profile')}
+                    name="externalProfile"
+                  />
+                </div>
+                <div className="w-1/2 pr-3 flex justify-center items-center">
+                  <Button
+                    as="a"
+                    href={`${process.env.NEXT_PUBLIC_AUTH_URL}/realms/edu-hub/account`}
+                    target="_blank"
+                    filled
+                    inverted
+                  >
+                    {t('change-password')}
+                  </Button>
+                </div>
+              </div>
+              {/* <FormFieldRow name="picture" type="file" /> */}
               <Button
                 as="button"
                 type="submit"
                 disabled={isSubmitting}
                 filled
                 inverted
-                className="block mx-auto mb-5 disabled:bg-slate-500"
+                className="mt-8 block mx-auto mb-5 disabled:bg-slate-500"
               >
-                {isSubmitting ? 'speichert...' : 'speichern'}
+                {isSubmitting ? t('saving') : t('save')}
               </Button>
             </form>
           </FormProvider>
-          <Button
-            as="a"
-            href={`${process.env.NEXT_PUBLIC_AUTH_URL}/realms/edu-hub/login-actions/reset-credentials?client_id=account-console`}
-            target="_blank"
-            filled
-            inverted
-          >
-            {t('change-password')}
-          </Button>
         </>
       ) : (
         <div>Loading</div>
