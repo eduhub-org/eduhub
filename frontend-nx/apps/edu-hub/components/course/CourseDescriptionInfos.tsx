b763c71b0e9b8a1c76b2e48ca312eb10ba30fe9c
steffen74 <steffen@opencampus.sh>
2023-03-06 23:26:41 +0100

    profile picture preview size added

--- frontend-nx/apps/edu-hub/components/course/CourseDescriptionInfos.tsx
+++ frontend-nx/apps/edu-hub/components/course/CourseDescriptionInfos.tsx
@@ -10,21 +10,27 @@ interface IProps {
 
 export const CourseDescriptionInfos: FC<IProps> = ({ course }) => {
   // const { t, lang } = useTranslation('course-page');
+  const { t } = useTranslation();
 
   return (
-    <div className="flex flex-wrap mt-20 bg-edu-course-invited rounded-2xl p-4">
-      <div className="w-full md:w-1/2 p-6">
-        <h2 className="text-3xl font-semibold mb-6">
-          {course.headingDescriptionField1}
-        </h2>
-        <ReactMarkdown>{course.contentDescriptionField1}</ReactMarkdown>
+    <>
+      <div className="flex flex-wrap mt-20 bg-edu-course-invited rounded-2xl p-4">
+        <div className="w-full md:w-1/2 p-6">
+          <h2 className="text-3xl font-semibold mb-6">
+            {course.headingDescriptionField1}
+          </h2>
+          <ReactMarkdown>{course.contentDescriptionField1}</ReactMarkdown>
+        </div>
+        <div className="w-full md:w-1/2 p-6">
+          <h2 className="text-3xl font-semibold mb-6">
+            {course.headingDescriptionField2}
+          </h2>
+          <ReactMarkdown>{course.contentDescriptionField2}</ReactMarkdown>
+        </div>
       </div>
-      <div className="w-full md:w-1/2 p-6">
-        <h2 className="text-3xl font-semibold mb-6">
-          {course.headingDescriptionField2}
-        </h2>
-        <ReactMarkdown>{course.contentDescriptionField2}</ReactMarkdown>
+      <div className="flex flex-wrap mt-10 bg-white rounded-2xl p-4">
+        <ReactMarkdown>{t('course-page:general-course-info')}</ReactMarkdown>
       </div>
-    </div>
+    </>
   );
 };
