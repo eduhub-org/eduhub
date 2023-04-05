9e772cc97aab35e80713243a7f5295702bdb41ff
Christoph Krüger <christoph@opencampus.sh>
2023-04-05 10:16:00 +0200

    questionaire sent flag to session table

---
+++ backend/migrations/default/1680682409999_alter_table_public_Session_add_column_questionaire_sent/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."Session" add column "questionaire_sent" Boolean
+--  not null default 'false';
---
+++ backend/migrations/default/1680682443431_alter_table_public_Session_alter_column_questionaire_sent/up.sql
@@ -0,0 +1,1 @@
+comment on column "public"."Session"."questionaire_sent" is E'indicates whether a questionaire has been sent out yet';
