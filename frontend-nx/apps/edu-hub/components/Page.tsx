2bcc7da1fb8488bbb0dbbdaa05c36da4bcb475d6
Eric Stumper <eric.stumper@gmail.com>
2023-05-31 12:29:02 +0200

    adds automatic logout when refresh token has expired and fixes a few warnings

--- frontend-nx/apps/edu-hub/components/Page.tsx
+++ frontend-nx/apps/edu-hub/components/Page.tsx
@@ -1,4 +1,7 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+
+import useLogout from '../hooks/logout';
 
 import { Footer } from './Footer';
 import { Header } from './Header';
@@ -9,6 +12,15 @@ type PageProps = {
 };
 
 export const Page: FC<PageProps> = ({ children, className }: PageProps) => {
+  const { data: session, status } = useSession();
+  const logout = useLogout();
+
+  useEffect(() => {
+    if (status !== 'loading' && session?.error === 'RefreshAccessTokenError') {
+      logout();
+    }
+  }, [status, session, logout]);
+
   return (
     <>
       <div className={`flex flex-col ${className}`}>
