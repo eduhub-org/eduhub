15f21fcca3d4306703f540dc0ab3d02f0fdbcfa3
steffen74 <steffen@opencampus.sh>
2023-06-11 18:48:09 +0200

    frontend test change

--- infrastructure/application/04_keycloak.tf
+++ infrastructure/application/04_keycloak.tf
@@ -70,8 +70,8 @@ module "keycloak_service" {
   image        = "${var.region}-docker.pkg.dev/${var.project_id}/docker-repo/keycloak"
 
   service_labels = {
-    sha                                   = var.keycloak_sha
-    run.googleapis.com / startupProbeType = null
+    sha              = var.keycloak_sha
+    startupProbeType = null
   }
 
   limits = {
