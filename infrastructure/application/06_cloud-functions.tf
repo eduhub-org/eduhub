ad9805a8405be96e5fcf53aa4e9e14ca833aeb25
steffen74 <steffen@opencampus.sh>
2023-08-31 16:40:05 +0200

    terraform correction

--- infrastructure/application/06_cloud-functions.tf
+++ infrastructure/application/06_cloud-functions.tf
@@ -42,6 +42,7 @@ resource "google_cloudfunctions2_function" "call_python_function" {
   service_config {
     environment_variables = {
       ENVIRONMENT                  = var.environment
+      BUCKET_NAME                  = var.project_id
       HASURA_ENDPOINT              = "https://${local.hasura_service_name}.opencampus.sh/v1/graphql"
       HASURA_GRAPHQL_ADMIN_KEY     = var.hasura_graphql_admin_key
       HASURA_CLOUD_FUNCTION_SECRET = var.hasura_cloud_function_secret
