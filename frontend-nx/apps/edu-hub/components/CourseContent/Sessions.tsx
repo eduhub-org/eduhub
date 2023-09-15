f254425f74c68a393a7a90936a76db20b704f36b
steffen74 <steffen@opencampus.sh>
2023-09-16 00:15:24 +0200

    adds end time and speaker to session list

--- frontend-nx/apps/edu-hub/components/CourseContent/Sessions.tsx
+++ frontend-nx/apps/edu-hub/components/CourseContent/Sessions.tsx
@@ -4,28 +4,33 @@
 
   import { Course_Course_by_pk_Sessions as Session } from '../../queries/__generated__/Course';
   import { isLinkFormat } from '../../helpers/util';
+  import UserCard from './UserCard';
 
   interface SessionsProps {
     sessions: Session[];
   }
 
   export const Sessions: FC<SessionsProps> = ({ sessions }) => {
-    const { t, lang } = useTranslation('course-page');
+    const { t, lang } = useTranslation('course');
     const [showAllSessions, setShowAllSessions] = useState(false);
-    const visibleSessions = showAllSessions ? sessions : sessions.slice(0, 4);
+
+    const initiallyShownSessions = 4;
+
+    const visibleSessions = showAllSessions ? sessions : sessions.slice(0, initiallyShownSessions);
 
     return (
       <>
         {visibleSessions.length > 0 && (
           <>
             <span className="text-3xl font-semibold mt-24">
-              {t('courseContent')}
+              {t('course_content')}
             </span>
             <ul>
               {visibleSessions.map(
-                ({ startDateTime, endDateTime, title, SessionAddresses }, index) => (
+                ({ startDateTime, endDateTime, title, SessionAddresses, SessionSpeakers }, index) => (
                   <li key={index} className="flex mb-4">
-                    <div className="flex flex-col flex-shrink-0 mr-6">
+                    <div className="flex flex-wrap items-start flex-shrink-0 mb-2">
+                    <div className="flex flex-col mr-6">
                       <span className="block text-sm sm:text-lg font-semibold">
                         {startDateTime?.toLocaleDateString(lang, {
                           year: 'numeric',
@@ -45,9 +50,10 @@
                         }) ?? ''}
                       </span>
                     </div>
+                    </div>
                     <div className="flex flex-col">
-                      <span className="block text-sm sm:text-lg">{title}</span>
-                      <div className="flex items-center ml-0 pl-0">
+                      <span className="block text-sm sm:text-lg whitespace-nowrap sm:whitespace-normal">{title}</span>
+                      <div className="whitespace-nowrap ml-0 pl-0">
                         {SessionAddresses.map(({ address }, index) => (
                           <span
                             key={index}
@@ -80,28 +86,36 @@
                           </span>
                         ))}
                       </div>
+                      <div className="flex flex-col">
+                      {SessionSpeakers && SessionSpeakers.map((speaker, speakerIndex) => (
+                       <UserCard key={speakerIndex} className='flex items-center my-3' user={speaker.Expert.User} role={t('speaker')} size='medium'/>
+                      ))}
                     </div>
+                    </div>
+
                   </li>
                 )
               )}
             </ul>
-            {showAllSessions ? (
-              <button
-                className="text-white hover:underline italic text-sm flex items-center"
-                onClick={() => setShowAllSessions(false)}
-              >
-                {t('course-page:hide-sessions')}
-                <IoIosArrowUp className="ml-1" />
-              </button>
-            ) : (
-              <button
-                className="text-white hover:underline italic text-sm flex items-center"
-                onClick={() => setShowAllSessions(true)}
-              >
-                {t('course-page:show-all-sessions')}
-                <IoIosArrowDown className="ml-1" />
-              </button>
-            )}
+            {sessions.length > initiallyShownSessions && (
+  showAllSessions ? (
+    <button
+      className="text-white hover:underline italic text-sm flex items-center"
+      onClick={() => setShowAllSessions(false)}
+    >
+      {t('course:hide_sessions')}
+      <IoIosArrowUp className="ml-1" />
+    </button>
+  ) : (
+    <button
+      className="text-white hover:underline italic text-sm flex items-center"
+      onClick={() => setShowAllSessions(true)}
+    >
+      {t('course:show_all_sessions')}
+      <IoIosArrowDown className="ml-1" />
+    </button>
+  )
+)}
           </>
         )}
       </>
--- frontend-nx/apps/edu-hub/queries/__generated__/AchievementRecordListWithAuthors.ts
+++ frontend-nx/apps/edu-hub/queries/__generated__/AchievementRecordListWithAuthors.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { AchievementRecord_bool_exp, AchievementRecord_order_by, AchievementRecordRating_enum } from "./../../__generated__/globalTypes";
+import { AchievementRecord_bool_exp, AchievementRecord_order_by, AchievementRecordRating_enum, University_enum } from "./../../__generated__/globalTypes";
 
 // ====================================================
 // GraphQL query operation: AchievementRecordListWithAuthors
@@ -28,6 +28,18 @@ export interface AchievementRecordListWithAuthors_AchievementRecord_AchievementR
    * The user's profile picture
    */
   picture: string | null;
+  /**
+   * A link to an external profile, for example in LinkedIn or Xing
+   */
+  externalProfile: string | null;
+  /**
+   * The university the user is attending or workin at (only provided if he is a student or working in academia)
+   */
+  university: University_enum | null;
+  /**
+   * Name of the university the student is attending or working at (only provided if his/her university is not part of the provided list)
+   */
+  otherUniversity: string | null;
 }
 
 export interface AchievementRecordListWithAuthors_AchievementRecord_AchievementRecordAuthors {
