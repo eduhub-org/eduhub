e0bd8ec22609a09d3a182efa798a4e983b9900ab
Eric Stumper <eric.stumper@gmail.com>
2023-09-27 16:21:52 +0200

    fixed mistake in fb pixel and cookiebot script placement

--- frontend-nx/apps/edu-hub/pages/_app.tsx
+++ frontend-nx/apps/edu-hub/pages/_app.tsx
@@ -50,12 +50,24 @@ const MyApp: FC<AppProps & InitialProps> & {
       <ApolloProvider client={client}>
         {/* Global Site Code Pixel - Facebook Pixel */}
         <Script
-          id="Cookiebot"
-          src="https://consent.cookiebot.com/uc.js"
-          data-cbid="718dfb5e-b62c-4993-9c65-99ae483f5510"
-          data-blockingmode="auto"
+          id="fb-pixel"
+          data-cookieconsent="marketing"
           strategy="afterInteractive"
           type="text/plain"
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1775867059535400');
+            fbq('track', 'PageView');
+          `,
+          }}
         />
         <Component {...pageProps} />
       </ApolloProvider>
