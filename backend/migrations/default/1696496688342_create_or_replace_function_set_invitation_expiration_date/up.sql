7dbe9aff10bc0229a93c8756acf24644cb555bb5
steffen74 <steffen@opencampus.sh>
2023-10-05 17:49:07 +0200

    Improves trigger to set invitation expiration deadline

--- backend/migrations/default/1696496688342_create_or_replace_function_set_invitation_expiration_date/up.sql
+++ backend/migrations/default/1696496688342_create_or_replace_function_set_invitation_expiration_date/up.sql
@@ -1,18 +1,17 @@
-
-
 CREATE OR REPLACE FUNCTION "public"."set_invitation_expiration_date"()
 RETURNS TRIGGER AS $$
-DECLARE
-  _new record;
 BEGIN
-  _new := NEW;
-  _new."invitationExpirationDate" = NOW();
-  RETURN _new;
+  IF (TG_OP = 'INSERT' AND NEW.status = 'INVITED') OR
+     (TG_OP = 'UPDATE' AND NEW.status = 'INVITED') THEN
+    NEW."invitationExpirationDate" = NOW() + interval '2 days';
+  END IF;
+  RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
 CREATE TRIGGER "set_invitation_expiration_date_trigger"
-BEFORE INSERT ON "public"."CourseEnrollment"
-FOR EACH ROW 
+BEFORE INSERT OR UPDATE ON "public"."CourseEnrollment"
+FOR EACH ROW
 EXECUTE PROCEDURE "public"."set_invitation_expiration_date"();
-COMMENT ON TRIGGER "set_invitation_expiration_date_trigger" ON "public"."CourseEnrollment" 
-IS 'trigger to set default value of column "invitationExpirationDate" to two days after creation on row insert';
+COMMENT ON TRIGGER "set_invitation_expiration_date_trigger" ON "public"."CourseEnrollment"
+IS 'trigger to set default value of column "invitationExpirationDate" to two days after creation when status is set or updated to INVITED';
