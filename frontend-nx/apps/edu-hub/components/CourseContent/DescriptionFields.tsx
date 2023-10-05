9bad9cd5fef51472fad25441326db3a7d39c069c
steffen74 <steffen@opencampus.sh>
2023-10-05 19:25:30 +0200

    change translation reference to refactored name

--- frontend-nx/apps/edu-hub/components/CourseContent/DescriptionFields.tsx
+++ frontend-nx/apps/edu-hub/components/CourseContent/DescriptionFields.tsx
@@ -11,49 +11,33 @@ interface IProps {
 
 export const DescriptionFields: FC<IProps> = ({ course }) => {
   // const { t, lang } = useTranslation('course-page');
-  const { t } = useTranslation();
+  const { t } = useTranslation('course');
 
-  const isAchievementCertificatePossible = course.achievementCertificatePossible
+  const isAchievementCertificatePossible = course.achievementCertificatePossible;
 
   return (
     <>
       {(course.headingDescriptionField1 && course.contentDescriptionField1) ||
       (course.headingDescriptionField2 && course.contentDescriptionField2) ? (
         <div className="flex flex-wrap bg-edu-course-invited rounded-2xl p-4">
-          {course.headingDescriptionField1 ||
-          course.contentDescriptionField1 ? (
+          {course.headingDescriptionField1 || course.contentDescriptionField1 ? (
             <div
               className={`w-full md:w-1/2 p-6 ${
-                !course.headingDescriptionField2 &&
-                !course.contentDescriptionField2
-                  ? 'md:w-full'
-                  : ''
+                !course.headingDescriptionField2 && !course.contentDescriptionField2 ? 'md:w-full' : ''
               }`}
             >
-              <h2 className="text-3xl font-semibold mb-6">
-                {course.headingDescriptionField1}
-              </h2>
-              <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                {course.contentDescriptionField1}
-              </ReactMarkdown>
+              <h2 className="text-3xl font-semibold mb-6">{course.headingDescriptionField1}</h2>
+              <ReactMarkdown remarkPlugins={[remarkGfm]}>{course.contentDescriptionField1}</ReactMarkdown>
             </div>
           ) : null}
-          {course.headingDescriptionField2 ||
-          course.contentDescriptionField2 ? (
+          {course.headingDescriptionField2 || course.contentDescriptionField2 ? (
             <div
               className={`w-full md:w-1/2 p-6 ${
-                !course.headingDescriptionField1 &&
-                !course.contentDescriptionField1
-                  ? 'md:w-full'
-                  : ''
+                !course.headingDescriptionField1 && !course.contentDescriptionField1 ? 'md:w-full' : ''
               }`}
             >
-              <h2 className="text-3xl font-semibold mb-6">
-                {course.headingDescriptionField2}
-              </h2>
-              <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                {course.contentDescriptionField2}
-              </ReactMarkdown>
+              <h2 className="text-3xl font-semibold mb-6">{course.headingDescriptionField2}</h2>
+              <ReactMarkdown remarkPlugins={[remarkGfm]}>{course.contentDescriptionField2}</ReactMarkdown>
             </div>
           ) : null}
         </div>
@@ -61,9 +45,10 @@ export const DescriptionFields: FC<IProps> = ({ course }) => {
       {isAchievementCertificatePossible ? (
         <div className="flex flex-wrap mt-10 bg-white rounded-2xl p-4">
           <ReactMarkdown remarkPlugins={[remarkGfm]}>
-            {t('course:general_achievement_certificate_conditions').replace(/\n/g, '  \n')}
+            {t('generalAchievementCertificateConditions').replace(/\n/g, '  \n')}
           </ReactMarkdown>
         </div>
-      ) : null}    </>
+      ) : null}{' '}
+    </>
   );
 };
