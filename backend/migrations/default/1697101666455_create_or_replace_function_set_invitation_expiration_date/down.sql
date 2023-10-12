9c1d181d5d81231caf5a4fde68d1983d418e7a3f
steffen74 <steffen@opencampus.sh>
2023-10-12 11:49:25 +0200

    fixes setting of expiration deadline

---
+++ backend/migrations/default/1697101666455_create_or_replace_function_set_invitation_expiration_date/down.sql
@@ -0,0 +1,12 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION "public"."set_invitation_expiration_date"()
+-- RETURNS TRIGGER AS $$
+-- BEGIN
+--   IF (TG_OP = 'INSERT' AND NEW.status = 'INVITED' AND NEW."invitationExpirationDate" IS NULL) OR
+--      (TG_OP = 'UPDATE' AND NEW.status = 'INVITED' AND NEW."invitationExpirationDate" IS NULL) THEN
+--     NEW."invitationExpirationDate" = NOW() + interval '2 days';
+--   END IF;
+--   RETURN NEW;
+-- END;
+-- $$ LANGUAGE plpgsql;
