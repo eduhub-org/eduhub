47ac8b5637445fb043d62fbc9632f916c154a511
Eric Stumper <eric.stumper@gmail.com>
2024-02-13 16:17:44 +0100

    fixes global type error

--- frontend-nx/apps/edu-hub/types/tanstack.d.ts
+++ frontend-nx/apps/edu-hub/types/tanstack.d.ts
@@ -1,5 +1,6 @@
-declare module '@tanstack/table-core' {
+import type { RowData } from '@tanstack/react-table';
 
+declare module '@tanstack/table-core' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ColumnMeta<TData extends RowData, TValue> {
     width?: number;
