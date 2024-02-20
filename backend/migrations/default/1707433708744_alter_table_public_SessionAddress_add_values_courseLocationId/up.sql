0a909b3efc6c2416f3a5a7a10ee60c4f1b607a2c
steffen74 <steffen@opencampus.sh>
2024-02-20 09:00:51 +0100

    corrects data migration for session addresses (as run in console)

--- backend/migrations/default/1707433708744_alter_table_public_SessionAddress_add_values_courseLocationId/up.sql
+++ backend/migrations/default/1707433708744_alter_table_public_SessionAddress_add_values_courseLocationId/up.sql
@@ -1,12 +1,32 @@
-
--- Fill the temporary location field
+-- Step 1: Initially fill the temporary location field
 UPDATE "public"."SessionAddress"
 SET "location" = CASE
     WHEN "type" = 'URL' THEN 'ONLINE'
     ELSE 'KIEL'
 END;
 
--- Add the courseLocationId to the SessionAddress table using the location field
+-- Step 2: Update location for sessions of courses with a single CourseLocation
+UPDATE "public"."SessionAddress" SA
+SET "location" = (
+  SELECT CL."locationOption"
+  FROM "public"."CourseLocation" CL
+  WHERE CL."courseId" IN (
+    SELECT S."courseId"
+    FROM "public"."Session" S
+    WHERE S."id" = SA."sessionId"
+  )
+  GROUP BY CL."locationOption", CL."courseId"
+  HAVING COUNT(CL."locationOption") = 1
+)
+WHERE "sessionId" IN (
+  SELECT S."id"
+  FROM "public"."Session" S
+  JOIN "public"."CourseLocation" CL ON S."courseId" = CL."courseId"
+  GROUP BY S."id", CL."courseId"
+  HAVING COUNT(DISTINCT CL."locationOption") = 1
+);
+
+-- Step 3: Add the courseLocationId to the SessionAddress table using the updated location field
 UPDATE "public"."SessionAddress" SA
 SET "courseLocationId" = (
   SELECT CL."id"
