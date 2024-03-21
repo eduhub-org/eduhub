15d5c360a11f7204842b48a7eaf9449791b748fd
Eric Stumper <eric.stumper@gmail.com>
2024-03-21 15:48:35 +0100

    adds default social preview image to course detail page

--- frontend-nx/apps/edu-hub/pages/course/[courseId].tsx
+++ frontend-nx/apps/edu-hub/pages/course/[courseId].tsx
@@ -29,7 +29,7 @@ const CoursePage: FC = () => {
         <title>EduHub | opencampus.sh</title>
         <link rel="icon" href="/favicon.png" />
         <meta property="og:title" content="EduHub | opencampus.sh" />
-        <meta property="og:image" content="https://edu.opencampus.sh/images/meta-image.png" />
+        <meta property="og:image" content="https://edu.opencampus.sh/images/edu_WISE23_HeaderWebsitePreview.png" />
       </Head>
       <Page>{isSessionLoading ? <CircularProgress /> : <CourseContent id={id} />}</Page>
     </>
