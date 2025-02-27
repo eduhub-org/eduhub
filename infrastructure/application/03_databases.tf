36a65adccf47ccd630519b5f4d5a59220f25b0d1
steffen74 <steffen@opencampus.sh>
2025-02-27 17:24:49 +0100

    reorganizes secrets and IAM bincdings

--- infrastructure/application/03_databases.tf
+++ infrastructure/application/03_databases.tf
@@ -46,29 +46,6 @@ resource "google_sql_user" "keycloak" {
   instance = google_sql_database_instance.default.name
   password = var.keycloak_db_pw
 }
-# Create a variable for the password of the Keycloak database user in the Google secret manager 
-resource "google_secret_manager_secret" "keycloak_db_pw" {
-  provider  = google-beta
-  secret_id = "keycloak-db-pw"
-  replication {
-    auto {}
-  }
-}
-
-# Set a value for the password of the Keycloak database user in the above created variable
-resource "google_secret_manager_secret_version" "keycloak_db_pw" {
-  provider    = google-beta
-  secret      = google_secret_manager_secret.keycloak_db_pw.name
-  secret_data = var.keycloak_db_pw
-}
-# Grant the defined service account member permission to access the secret with the password for the Keycloak database
-resource "google_secret_manager_secret_iam_member" "keycloak_db_pw" {
-  secret_id  = google_secret_manager_secret.keycloak_db_pw.id
-  role       = "roles/secretmanager.secretAccessor"
-  member     = "serviceAccount:${data.google_project.eduhub.number}-compute@developer.gserviceaccount.com"
-  depends_on = [google_secret_manager_secret.keycloak_db_pw]
-}
-
 
 ###############################################################################
 # Create the Hasura database in the EduHub SQL instance and the user to access it
@@ -84,18 +61,4 @@ resource "google_sql_user" "hasura" {
   name     = var.hasura_db_user
   instance = google_sql_database_instance.default.name
   password = var.hasura_db_pw
-}
-# Create a secret for the URL of the Hasura database in the Google secret manager 
-resource "google_secret_manager_secret" "hasura_db_url" {
-  provider  = google-beta
-  secret_id = "hasura-db-url"
-  replication {
-    auto {}
-  }
-}
-# Create a new version of the secret for the URL to the Hasura database based on the user and password created for the Hasura database
-resource "google_secret_manager_secret_version" "hasura_db_url" {
-  provider    = google-beta
-  secret      = google_secret_manager_secret.hasura_db_url.name
-  secret_data = "postgres://${var.hasura_db_user}:${var.hasura_db_pw}@${google_sql_database_instance.default.private_ip_address}:5432/${google_sql_database.hasura.name}"
 }
